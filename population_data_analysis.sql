bb80224c4912b949d2142316658f15c5039d6e45
Omkar078 <omkarpanchal6789@gmail.com>
2023-03-15 18:55:43 +0530

    Update population_data_analysis.sql

--- population_data_analysis.sql
+++ population_data_analysis.sql
@@ -17,16 +17,22 @@ SELECT SUM(Population) AS Total_Population FROM dbo.data2
 SELECT AVG(Growth)*100 AS average_growth FROM dbo.data1
 
 --Average Growth of State
-SELECT State, AVG(Growth)*100 AS avg_growth FROM dbo.data1
+SELECT State, ROUND(AVG(Growth)*100, 2) AS avg_growth FROM dbo.data1
 GROUP BY State
 ORDER BY AVG(Growth) DESC
 
 --Average sex ration as per the state
-SELECT State, AVG(Sex_Ratio)*100 AS avg_sex_ratio FROM dbo.data1
+SELECT State, ROUND(AVG(Sex_Ratio)*100, 0) AS avg_sex_ratio FROM dbo.data1
 GROUP BY State
 ORDER BY AVG(Sex_Ratio) DESC
 
 -- Average Literacy Rate
-SELECT State, AVG(Literacy)*100 AS avg_literacy_rate FROM dbo.data1
+SELECT State, ROUND(AVG(Literacy)*100, 0) AS avg_literacy_rate FROM dbo.data1
 GROUP BY State
+ORDER BY AVG(Literacy)*100 DESC
+
+--Find out States with literacy rate greater than 90
+SELECT State, ROUND(AVG(Literacy)*100, 0) AS avg_literacy_rate FROM dbo.data1
+GROUP BY State 
+HAVING ROUND(AVG(Literacy)*100, 0)>90
 ORDER BY AVG(Literacy)*100 DESC
