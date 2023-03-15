d45476f6a8e2b03254f6cb618b998c4169db62b3
Omkar078 <omkarpanchal6789@gmail.com>
2023-03-15 17:03:12 +0530

    Update population_data_analysis.sql

--- population_data_analysis.sql
+++ population_data_analysis.sql
@@ -1,3 +1,13 @@
+SELECT * FROM dbo.data1
+SELECT * FROM dbo.data2
 
+-- counting rows in each table
+SELECT COUNT(*) FROM dbo.data1
+SELECT COUNT(*) FROM dbo.data2
 
-select * from dbo.Data1$
+--Data for Maharashtra & Telangana
+SELECT * FROM dbo.data1 WHERE State IN ('Maharashtra', 'Bihar')
+SELECT * FROM dbo.data2 WHERE State IN ('Maharashtra', 'Bihar')
+
+--Population of India
+SELECT SUM(Population) AS Total_Population FROM dbo.data2
